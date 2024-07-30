f8b5cc4a2184ad6114ef55e00d155d42095f102e
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-07-30 20:50:49 +0530

    LeetCode-852-PeakIndexInMountainArray.java

--- src/com/java/dsa/binarySearch/PeakIndexInMountainArray.java
+++ src/com/java/dsa/binarySearch/PeakIndexInMountainArray.java
@@ -4,14 +4,13 @@ class PeakIndexInMountainArray {
          System.out.println("Peak Element is " + findPeakIndexInMountainArray(arr));
     }
     static int findPeakIndexInMountainArray(int[] arr) {
-        int low = 0, high = arr.length - 1, ans = -1;
+        int low = 0, high = arr.length - 1;
         while (low < high) {
             int mid = low + (high - low) / 2;
             if (arr[mid] > arr[mid + 1]) {
                 // you are in descending part of array. 
                 // This may be the answer but look at left to see if there is another descending element
-                ans = mid;
-                high = mid - 1;
+                high = mid;
             } else {
                 // You are in ascending part of array
                 // because we know that mid + 1 element is greater than mid element
@@ -23,6 +22,6 @@ class PeakIndexInMountainArray {
         // hence when they both are finding to just one element that is the maximum one
         // More elaboration: At every point of time for low and high they have best possible answer till that time
         // and if we are saying if only one item is remaining hence that's the best possible answer
-        return ans;
+        return low;
     }
 }

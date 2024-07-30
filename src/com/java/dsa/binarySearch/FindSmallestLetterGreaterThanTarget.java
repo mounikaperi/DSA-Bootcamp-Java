324c78ee524a06a4c1da62668f1aff5630b9c01d
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-07-30 15:30:09 +0530

    LeetCode744-FindSmallestLetterGreaterThanTarget.java
    LeetCode 744
    https://leetcode.com/problems/find-smallest-letter-greater-than-target/

---
+++ src/com/java/dsa/binarySearch/FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+class FindSmallestLetterGreaterThanTarget {
+    public static void main(String[] args) {
+        char[] arr = {'c', 'f', 'g'};
+        char target = 'a';
+        System.out.println("Smallest letter that is greater than target " +target+ " is " + findLetter(arr, target));
+    }
+    static char findLetter(char[] arr, char target) {
+        if (target > arr[arr.length - 1])
+            return arr[0];
+        int low = 0; int high = arr.length-1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if (target > arr[mid]) low = mid + 1;
+            else high = mid - 1;
+        }
+        return  arr[low];
+    }
+}

7c43d56dea0d50101012c18a29e4b3df5293e148
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-07-31 14:03:57 +0530

    LeetCode-33-RotatedBinarySearchLongVersion

--- src/com/java/dsa/binarySearch/RotatedBinarySearchLongVersion.java
+++ src/com/java/dsa/binarySearch/RotatedBinarySearchLongVersion.java
@@ -1,5 +1,5 @@
 // https://leetcode.com/problems/search-in-rotated-sorted-array/
-class RotatedBinarySearch {
+class RotatedBinarySearchLongVersion {
     public static void main(String[] args) {
         int[] arr = {4, 5, 6, 7, 0, 1, 2};
         int target = 0;

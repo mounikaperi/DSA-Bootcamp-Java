489897844e243d9dfe6b8ffd2581418f48081588
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-08-08 19:36:32 +0530

    LeetCode-1480-RunningSumOf1D.java
    https://leetcode.com/problems/running-sum-of-1d-array/description/

---
+++ src/com/java/dsa/arrays/RunningSumOf1D.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class RunningSumOf1D {
+    public static void main(String[] args) {
+        // https://leetcode.com/problems/running-sum-of-1d-array/description/
+        int[] arr = {1, 2, 3, 4};
+        System.out.println("Smaller elements in array for each element: " + Arrays.toString(runningSum(arr)));
+    }
+    public static int[] runningSum(int[] nums) {
+        for (int i=1; i<nums.length; i++) {
+            nums[i] += nums[i-1];
+        }
+        return nums;
+    }
+}

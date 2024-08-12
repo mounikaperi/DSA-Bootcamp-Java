f8bc0a837fa1e385bf3c8de96c93ec29d0046027
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-08-12 09:57:14 +0530

    Update LowerBound.java

--- src/com/java/dsa/binarySearch/LowerBound.java
+++ src/com/java/dsa/binarySearch/LowerBound.java
@@ -1,8 +1,9 @@
-public class LowerBound {
+public class Main {
 	public static void main(String[] args) {
 		int[] arr = {1, 2, 2, 3, 3, 5};
 		int x = 2;
 		System.out.println("Lower Bound of x is: " + findLowerBound(arr, x));
+		System.out.println("Lower Bound of x is: " + findLowerBoundViaRecursion(arr, 0, arr.length-1, x));
 	}
 	private static int findLowerBound(int[] arr, int target) {
 	    int low = 0;
@@ -18,5 +19,17 @@ public class LowerBound {
 	        }
 	    }
 	    return ans;
+	}
+	private static int findLowerBoundViaRecursion(int[] arr, int low, int high, int target) {
+	    int ans = high + 1;
+            if (low > high) return ans;
+            int mid = (low + high)/2;
+            if (arr[mid] >= target) {
+               ans = mid;
+	       ans = findLowerBoundViaRecursion(arr, low, mid-1, target);
+            } else {
+               ans = findLowerBoundViaRecursion(arr, mid+1, high, target);
+            } 
+            return ans;
 	}
 }

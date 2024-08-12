a5dbd881a9c1bdf67236608ae881736ebfb811c7
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-08-12 15:01:50 +0530

    Create IsNumberPerfectSquare.java

---
+++ src/com/java/dsa/binarySearch/IsNumberPerfectSquare.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class IsNumberPerfectSquare {
+    public static void main(String[] args) {
+        // https://leetcode.com/problems/valid-perfect-square/
+        int num = 16;
+        System.out.println("Is the number " +num+ " a perfect square? " + isPerfectSquare(num));
+    }
+    private static boolean isPerfectSquare(int num) {
+        int low = 1;
+        int high = num;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if ((long) (mid * mid) == num) return true;
+            else if ((long) (mid*mid) < num) low = mid + 1;
+            else high = mid - 1;
+        }
+        return false;
+    }
+}
+

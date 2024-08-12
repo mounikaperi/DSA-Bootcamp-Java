cc8e4e16c9b789f868310f5fab5937202c902df3
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-08-12 16:35:50 +0530

    Create kthMissingNumber.java

---
+++ src/com/java/dsa/binarySearch/kthMissingNumber.java
@@ -0,0 +1,20 @@
+public class kthMissingNumber {
+    public static void main(String[] args) {
+        // https://leetcode.com/problems/kth-missing-positive-number/description/
+        int[] arr = {2,3,4,7,11};
+        int target = 5;
+        System.out.println(target +"th missing number is: " + kthMissingNumber(arr, target));
+    }
+    private static int kthMissingNumber(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length-1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int missingCount = arr[mid] - (mid + 1);
+            if (missingCount > target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return low + target;
+    }       
+}
+

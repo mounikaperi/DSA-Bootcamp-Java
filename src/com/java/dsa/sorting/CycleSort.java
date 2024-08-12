fca5c3e55394c06d2b79d4d5619bb3ec89857812
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-08-12 18:40:10 +0530

    Create CycleSort.java

---
+++ src/com/java/dsa/sorting/CycleSort.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class CycleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 1, 4, 3};
+        System.out.println("Sorted Array is: " + Arrays.toString(cycleSort(arr)));
+    }
+    private static int[] cycleSort(int[] arr) {
+        int i=0;
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+        return arr;
+    }    
+    private static void swap(int[] arr, int i, int correctIndex) {
+        int temp = arr[i];
+        arr[i] = arr[correctIndex];
+        arr[correctIndex] = temp;
+    }
+}

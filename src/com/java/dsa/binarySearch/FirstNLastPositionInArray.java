b8e15b0b311b67fd19cec7c0dba28b486ec2ba99
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-08-12 11:06:52 +0530

    Update FirstNLastPositionInArray.java

--- src/com/java/dsa/binarySearch/FirstNLastPositionInArray.java
+++ src/com/java/dsa/binarySearch/FirstNLastPositionInArray.java
@@ -1,9 +1,10 @@
 public class FirstNLastPositionInArray {
 	public static void main(String[] args) {
+		// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
 		int[] arr = {5,7,7,8,8,10};
-        int target = 8;
-        int[] searchRange = searchRange(arr, target);
-        System.out.println("First Occurrence is: " + searchRange[0] + " Last Occurrence is: " + searchRange[1]);
+	        int target = 8;
+	        int[] searchRange = searchRange(arr, target);
+	        System.out.println("First Occurrence is: " + searchRange[0] + " Last Occurrence is: " + searchRange[1]);
 	}
 	private static int[] searchRange(int[] arr, int target) {
 	    int ans[] = {-1, -1};

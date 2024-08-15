cd06821fa1c63b54fed62ff5a46799e37cb97c10
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-08-15 18:04:43 +0530

    Update SearchTargetElement.java

--- src/com/java/dsa/recursion/SearchTargetElement.java
+++ src/com/java/dsa/recursion/SearchTargetElement.java
@@ -1,12 +1,27 @@
 public class SearchTargetElement {
 	public static void main(String[] args) {
-	    int[] arr = {10, 1, 9, 2, 3, 8, 7, 4, 6, 5};
+	    int[] arr = {10, 1, 9, 2, 3, 8, 7, 4, 6, 5, 8};
 	    int target = 8;
 	    boolean isTargetFound = findTarget(arr, target, 0);
 		System.out.println("Is Target Found: " + isTargetFound);
+		int index = findIndex(arr, target, 0);
+		System.out.println("The index at which element is found: " + index);
+		int lastIndex = findLastIndex(arr, target, arr.length-1);
+		System.out.println("The last index at which element is found: " + lastIndex);
 	}
 	private static boolean findTarget(int[] arr, int target, int index) {
 	    if (index == arr.length) return false;
 	    return arr[index] == target || findTarget(arr, target, index+1);
 	}
+	private static int findIndex(int[] arr, int target, int index) {
+	    if (index == arr.length) return -1;
+	    if (arr[index] == target) return index;
+	    return findIndex(arr, target, index+1);
+	}
+	private static int findLastIndex(int[] arr, int target, int index) {
+	    if (index == -1) return -1;
+	    if (arr[index] == target) return index;
+	    return findLastIndex(arr, target, index-1);
+	}
+	
 }

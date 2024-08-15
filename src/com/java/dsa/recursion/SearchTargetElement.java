361c9675f7924bc7eda5c9a7644f69f7686c9787
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-08-15 18:18:52 +0530

    Update SearchTargetElement.java

--- src/com/java/dsa/recursion/SearchTargetElement.java
+++ src/com/java/dsa/recursion/SearchTargetElement.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 public class SearchTargetElement {
+    static ArrayList<Integer> list = new ArrayList<>();
 	public static void main(String[] args) {
 	    int[] arr = {10, 1, 9, 2, 3, 8, 7, 4, 6, 5, 8};
 	    int target = 8;
@@ -8,6 +10,8 @@ public class SearchTargetElement {
 		System.out.println("The index at which element is found: " + index);
 		int lastIndex = findLastIndex(arr, target, arr.length-1);
 		System.out.println("The last index at which element is found: " + lastIndex);
+		ArrayList<Integer> resultList = findAllIndices(arr, target, 0);
+		System.out.println("The indices matching the target element is: " + list);
 	}
 	private static boolean findTarget(int[] arr, int target, int index) {
 	    if (index == arr.length) return false;
@@ -23,5 +27,10 @@ public class SearchTargetElement {
 	    if (arr[index] == target) return index;
 	    return findLastIndex(arr, target, index-1);
 	}
-	
+	private static ArrayList<Integer> findAllIndices(int[] arr, int target, int index) {
+	    if (index == arr.length) return list;
+	    if (arr[index] == target) list.add(index);
+	    findAllIndices(arr, target, index+1);
+	    return list;
+	}
 }

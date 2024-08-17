113e89d3a0492fe4beda4c4f7e0e12cbe863ee03
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-08-17 08:49:46 +0530

    Update TrianglePattern.java

--- src/com/java/dsa/recursion/TrianglePattern.java
+++ src/com/java/dsa/recursion/TrianglePattern.java
@@ -1,15 +1,26 @@
 public class TrianglePattern {
 	public static void main(String[] args) {
+		reverseTriangle(4, 0);
 		triangle(4, 0);
+	}
+	private static void reverseTriangle(int row, int col) {
+	    if (row == 0) return;
+	    if (col < row) {
+	        System.out.print("*");
+	        reverseTriangle(row, col+1);
+	    } else {
+	        System.out.println();
+	        reverseTriangle(row-1, 0);
+	    }
 	}
 	private static void triangle(int row, int col) {
 	    if (row == 0) return;
 	    if (col < row) {
-	        System.out.print("*");
 	        triangle(row, col+1);
+	        System.out.print("*");
 	    } else {
-	        System.out.println();
 	        triangle(row-1, 0);
+	        System.out.println();
 	    }
 	}
 }

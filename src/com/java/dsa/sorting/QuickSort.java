6255b6bf3b8d4bbc9fcb0f526ff0c92aa401fa27
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-08-25 18:36:30 +0530

    Create QuickSort.java

---
+++ src/com/java/dsa/sorting/QuickSort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+public class QuickSort {
+	public static void main(String[] args) {
+	    int[] arr = {5, 4, 3, 2, 1};
+		System.out.println("Array before sorting: " + Arrays.toString(arr));
+		quickSort(arr, 0, arr.length-1);
+		System.out.println("Array after sorting: " + Arrays.toString(arr));
+	}
+	private static void quickSort(int[] arr, int low, int high) {
+	    if (low >= high) return;
+	    int start = low;
+	    int end = high;
+	    int mid = start + (end - start) / 2;
+	    int pivot = arr[mid];
+	    while (start <= end) {
+	        while (arr[start] < pivot) start++;
+	        while (arr[end] > pivot) end--;
+	        if (start <= end) {
+	            int temp = arr[start];
+	            arr[start] = arr[end];
+	            arr[end] = temp;
+	            start++;
+	            end--;
+	        }
+	    }
+	    quickSort(arr, low, end);
+	    quickSort(arr, start, high);
+	}
+}

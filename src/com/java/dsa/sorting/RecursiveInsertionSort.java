619933cd09a89e69f76ac576b4395c1a88de5f78
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-08-25 18:41:59 +0530

    Create RecursiveInsertionSort.java

---
+++ src/com/java/dsa/sorting/RecursiveInsertionSort.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class RecursiveInsertionSort {
+    static void insertionSort(int[] arr, int i, int n) {
+        // Base Case: i == n.
+        if (i == n) return;
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+        insertionSort(arr, i + 1, n);
+
+    }
+    public static void main(String args[]) {
+        int arr[] = {13, 46, 24, 52, 20, 9};
+        int n = arr.length;
+        System.out.println("Before Using insertion Sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        insertionSort(arr, 0, n);
+        System.out.println("After insertion sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}

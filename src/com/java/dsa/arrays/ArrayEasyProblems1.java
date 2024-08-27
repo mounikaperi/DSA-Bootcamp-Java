437aa1ec39325aa0babad063623395312bf0d4c2
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-08-27 16:48:57 +0530

    Update and rename LargestSmallestElementsInArray.java to ArrayEasyProblems1.java

--- src/com/java/dsa/arrays/ArrayEasyProblems1.java
+++ src/com/java/dsa/arrays/ArrayEasyProblems1.java
@@ -1,6 +1,7 @@
-public class LargestSmallestElementsInArray {
+import java.util.Arrays;
+public class ArrayEasyProblems1 {
 	public static void main(String[] args) {
-	    int[] arr = {100, 90, 80, 10, 20, 50, 70, 60, 30, 40};
+	    int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 	    int largestElement = getLargestElement(arr);
 	    System.out.println("The largest element in array is: " + largestElement);
 	    int smallestElement = getSmallestElement(arr);
@@ -9,6 +10,13 @@ public class LargestSmallestElementsInArray {
 	    System.out.println("The second largest element in array is: " + secondLargestElement);
 	    int secondSmallestElement = getSecondSmallestElement(arr);
 	    System.out.println("The second smallest element in array is: " + secondSmallestElement);
+	    boolean isArraySorted = IsInputArraySorted(arr);
+	    System.out.println("Is Array sorted: " + isArraySorted);
+	    boolean isArrayRotated = isInputArraySortedNRotated(arr);
+	    System.out.println("Is Array rotated: " + isArrayRotated);
+	    int[] arr1 = {10, 10, 20, 20, 30, 40, 50};
+	    int[] result = removeDuplicates(arr1);
+	    System.out.println("Array after removing duplicates: " + Arrays.toString(result));
 	}
 	private static int getLargestElement(int[] arr) {
 	    int largestElement = arr[0];
@@ -52,5 +60,32 @@ public class LargestSmallestElementsInArray {
 	        }
 	    }
 	    return secondSmallestElement;
+	}
+	private static boolean IsInputArraySorted(int[] arr) {
+	    if (arr.length == 0 || arr.length == 1) return true;
+	    for (int i=0; i<arr.length-1; i++) {
+	        if (arr[i] > arr[i+1] && arr[i] != arr[i+1]) return false;
+	    }
+	    return true;
+	}
+	private static boolean isInputArraySortedNRotated(int[] arr) {
+	   int rotationCount = 0;
+	   int n = arr.length;
+	   for (int i=0; i<n; i++) {
+	       if (arr[i] > arr[(i+1) % n]) {
+	           ++rotationCount;
+	       }
+	   }
+	   return rotationCount <= 1;
+	}
+	private static int[] removeDuplicates(int[] arr) {
+	    int i=0;
+	    for (int j=1; j<arr.length; j++) {
+	        if (arr[i] != arr[j]) {
+	            i++;
+	            arr[i] = arr[j];
+	        }
+	    }
+	    return Arrays.copyOfRange(arr, 0, i+1);
 	}
 }

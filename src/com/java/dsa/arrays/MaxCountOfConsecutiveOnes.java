b1395ce0067693f5e335176ac813eea1d507c83b
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-08-27 19:53:47 +0530

    Update MaxCountOfConsecutiveOnes.java

--- src/com/java/dsa/arrays/MaxCountOfConsecutiveOnes.java
+++ src/com/java/dsa/arrays/MaxCountOfConsecutiveOnes.java
@@ -4,15 +4,18 @@ public class MaxCountOfConsecutiveOnes {
 	    System.out.println("Count of maximum consecutive ones: " + maxCountOfConsecutiveOnes(arr));
 	}
 	private static int maxCountOfConsecutiveOnes(int[] arr) {
-	    int count = 0; int maxCount = 0;
-	    for (int i=0; i<arr.length; i++) {
-	        if (arr[i] == 1) {
-	            count++;
-	        } else {
-	            count = 0;
+	        int n = arr.length;
+	        int max = 0;
+	        int current = 0; 
+	        for (int i = 0; i < n; i++) {
+	            if (arr[i] == 1) {
+	                current++;
+	            } else {
+	                max = Math.max(max, current);
+	                current = 0;
+	            }
 	        }
-	        maxCount = Math.max(maxCount, count);
-	    }
-	    return maxCount;
+	        max = Math.max(max, current);
+	        return max;
 	}
 }

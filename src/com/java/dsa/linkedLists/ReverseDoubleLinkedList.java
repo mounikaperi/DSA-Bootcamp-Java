a2771540138413ef3446008c18fa00e2ebc09ae6
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-09-06 09:12:44 +0530

    Create ReverseDoubleLinkedList.java

---
+++ src/com/java/dsa/linkedLists/ReverseDoubleLinkedList.java
@@ -0,0 +1,26 @@
+class ReverseDoubleLinkedList {
+    public DLLNode reverseDLL(DLLNode head) {
+        // Your code here
+        if (head == null || head.next == null) return head;
+        DLLNode prev = null;
+        DLLNode current = head;
+        while (current!=null) {
+            prev = current.prev;
+            current.prev = current.next;
+            current.next = prev;
+            current = current.prev;
+        }
+        return prev.prev;
+    }
+    class DLLNode {
+      int data;
+      DLLNode next;
+      DLLNode prev;
+  
+      DLLNode(int val) {
+          data = val;
+          next = null;
+          prev = null;
+      }
+   }
+}

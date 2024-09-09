8e3ea50c3b43c590e22e2fc1f84d95bda4fcbc51
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-09-09 07:55:44 +0530

    Create BinaryTreeImplementation.java

---
+++ src/com/java/dsa/binaryTrees/BinaryTreeImplementation.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+public class BinaryTreeImplementation {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BinaryTree tree = new BinaryTree();
+		tree.populate(sc);
+		tree.display();
+	}
+}
+class BinaryTree {
+    public BinaryTree() {
+        
+    }
+    private static class Node {
+        int value;
+        Node left;
+        Node right;
+        public Node(int value) {
+           this.value = value; 
+        }
+    }
+    private Node root;
+    // insert elements
+    public void populate(Scanner sc) {
+        System.out.println("Enter the root node: ");
+        int value = sc.nextInt();
+        root = new Node(value);
+        populate(sc, root);
+    }
+    private void populate(Scanner sc, Node node) {
+        System.out.println("Do you want to enter left of " + node.value);
+        boolean left = sc.nextBoolean();
+        if (left) {
+            System.out.println("Enter the value of the left node: " + node.value);
+            int value = sc.nextInt();
+            node.left = new Node(value);
+            populate(sc, node.left);
+        }
+        System.out.println("Do you want to enter right of " + node.value);
+        boolean right = sc.nextBoolean();
+        if (right   ) {
+            System.out.println("Enter the value of the right node: " + node.value);
+            int value = sc.nextInt();
+            node.right = new Node(value);
+            populate(sc, node.right);
+        }
+    }
+    public void display() {
+        display(root, "");
+    }
+    private void display(Node node, String indent) {
+        if (node == null) return;
+        System.out.println(indent + node.value);
+        display(node.left,  indent + "\t");
+        display(node.right, indent + "\t");
+    }
+}

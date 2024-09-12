236d33638499fd6ad38ab996559dc1d6a954e4a0
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-09-12 16:05:39 +0530

    ValidateBinarySearchTree

---
+++ src/com/java/dsa/trees/FlattenBinaryTree.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class FlattenBinaryTree {
+    public static void main(String[] args) {
+      // https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+      TreeNode root = new TreeNode(1);
+      root.left = new TreeNode(2);
+      root.left.left = new TreeNode(3);
+      root.left.right = new TreeNode(4);
+      root.right = new TreeNode(2);
+      root.right.left = new TreeNode(4);
+      root.right.right = new TreeNode(3);
+      int[] nums = {1, 2, 3, 4, 5, 6};
+      TreeNode root = flattenBinaryTree(nums);
+    }
+    private static TreeNode flattenBinaryTree(TreeNode node) {
+      if (node == null) return null;
+      TreeNode current = root;
+      while (current != null) {
+        if (current.left != null) {
+          TreeNode rightMost = current.left;
+          while (rightMost.right != null) {
+            rightMost = rightMost.right;
+          }
+          rightMost.right = current.right;
+          current.right = current.left;
+          current.left = null;
+        }
+        current = current.right;
+      }
+    }
+}
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+  TreeNode(int val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}

545b4009a596f2fa315de386102aa422560bb7e1
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-09-15 16:05:00 +0530

    LongestSubstringWithoutRepeatingCharacters

---
+++ src/com/java/dsa/slidingwindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+  public static void main(String[] args) {
+    // https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+    String s = "abcabcbb";
+    System.out.println("BruteForce: The length of the longest substring is: " + lengthOfLongestSubstring(s)); 
+    System.out.println("SlidingWindow: The length of the longest substring is: " + lengthOfLongestSubstringUsingSlidingWindow(s)); 
+  }
+  private static int lengthOfLongestSubstring(String s) { // O(n^2)
+    int maxLength = 0;
+    for (int i=0; i<s.length()-1; i++) {
+      int[] hash = new int[256];
+      for (int j=i; j<s.length()-1; j++) {
+        if (hash[s.charAt(j)] == 1) break;
+        maxLength = Math.max(maxLength, j-i+1);
+        hash[s.charAt(j)] = 1;
+      }
+    }
+    return maxLength;
+  }
+  private static int lengthOfLongestSubstringUsingSlidingWindow(String s) {
+    int[] hash = new int[256];
+    Arrays.fill(hash, -1);
+    int left = 0;
+    int right = 0;
+    int maxLength = 0;
+    while (right < s.length()-1) {
+      if (hash[s.charAt(right)] != -1) {
+        if (hash[s.charAt(right)] >= left) {
+          left = hash[s.charAt(right)] + 1;
+        }
+      }
+      maxLength = Math.max(maxLength, right-left+1);
+      hash[s.charAt(right)] = right;
+      right++;
+    }
+    return maxLength;
+  }
+}

cfd57ef45869ec82776a3cc853c84c47d2b2f52c
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-10-05 12:17:29 +0530

    Stocks

---
+++ src/com/java/dsa/arrays/BestTimeToBuyNSellStock.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class BestTimeToBuyNSellStock {
+    public static void main(String[] args) {
+      int[] prices = {7,1,5,3,6,4};
+      int maxProfit = findMaxProfit(prices);
+      System.out.println("Max profit is: " + maxProfit);
+    }
+    private static int findMaxProfit(int[] prices) {
+      int maxProfit = 0;
+      int minStock = prices[0];
+      for (int i=1; i<prices.length; i++) {
+        minStock = Math.min(minStock, prices[i]);
+        maxProfit = Math.max(maxProfit, prices[i] - minStock);
+      }
+      return maxProfit;
+    }
+}

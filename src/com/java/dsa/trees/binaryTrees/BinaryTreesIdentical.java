0d3923f0a24ae5713a12f83815bc4422d6664720
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-11-16 21:09:19 +0530

    BinaryTreesIdentical

---
+++ src/com/java/dsa/trees/binaryTrees/BinaryTreesIdentical.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class BinaryTreesIdentical {
+    // https://leetcode.com/problems/same-tree/description/
+    public static void main(String[] args) {
+      TreeNode root1 = new TreeNode(1);
+      root1.left = new TreeNode(2);
+      root1.right = new TreeNode(3);
+      root1.left.left = new TreeNode(4);
+      
+      TreeNode root2 = new TreeNode(1);
+      root2.left = new TreeNode(2);
+      root2.right = new TreeNode(3);
+      root2.left.left = new TreeNode(4);
+      
+      if (isIdentical(root1, root2)) {
+        System.out.println("The binary trees are identical.");
+      } else {
+        System.out.println("The binary trees are not identical.");
+      }
+    }
+    private static boolean isIdentical(TreeNode node1, TreeNode node2) {
+      if (node1 == null && node2 == null) return true;
+      if (node1 == null || node2 == null) return false;
+      return (
+        (node1.data == node2.data) && isIdentical(node1.left, node2.left) && isIdentical(node1.right, node2.right)
+        );
+    }
+}
+class TreeNode {
+  int data;
+  TreeNode left;
+  TreeNode right;
+  public TreeNode(int val) {
+    data = val;
+    left = null;
+    right = null;
+  }
+}

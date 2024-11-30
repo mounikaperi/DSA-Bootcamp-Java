7e08f5a3b88ef9b7a2a7976d54237afbd5ea8849
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-11-30 16:50:13 +0530

    SortCharactersByFrequency

---
+++ src/com/java/dsa/strings/SortCharactersByFrequency.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class SortCharactersByFrequency {
+    public String frequencySort(String s) {
+        Map<Character, Integer> frequencyMap = new HashMap<>(52); // 26 lowercase, 26 uppercase
+        for (int i=0; i<s.length(); i++) {
+            // Merge the current character into the map, increasing its count by 1
+            frequencyMap.merge(s.charAt(i), 1, Integer::sum);
+        }
+        List<Character> characters = new ArrayList<>(frequencyMap.keySet()); // for sorting
+        characters.sort((a, b) -> frequencyMap.get(b) - frequencyMap.get(a));
+        StringBuilder sortedString = new StringBuilder();
+        for (char c : characters) {
+            // Append each character to the StringBuilder based on its frequency
+            for (int frequency = frequencyMap.get(c); frequency > 0; --frequency) {
+                sortedString.append(c);
+            }
+        }
+        return sortedString.toString();
+    }
+    public static void main(String[] args) {
+      Sting s = "cccaaa";
+      String result = frequencySort(s);
+      System.out.println("The sorted string is: " + result);
+    }
+}

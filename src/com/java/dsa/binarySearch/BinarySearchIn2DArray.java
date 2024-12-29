cae79cc39474fd2c8d930c6e898e5dbbb4b9f202
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-12-29 14:11:20 +0530

    searchMatrixOptimal

--- src/com/java/dsa/binarySearch/BinarySearchIn2DArray.java
+++ src/com/java/dsa/binarySearch/BinarySearchIn2DArray.java
@@ -4,6 +4,10 @@ class BinarySearchIn2DArray {
        int arr[][] = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
        int target = 3;
        System.out.println("Is Element present? "+ searchMatrix(arr, target));
+
+       int[][] mat = {{1, 5, 9}, {14, 20, 21}, {30, 34, 43}};
+       int x = 14;
+       System.out.println("Is Element present? "+ searchMatrixOptimal(arr, target));
     }
     static boolean searchMatrix(int[][] matrix, int target) {
         for (int i=0; i<matrix.length; i++) {
@@ -23,4 +27,19 @@ class BinarySearchIn2DArray {
         }
         return false;
     }
+    static boolean searchMatrixOptimal(int[][] mat, int target) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int low = 0; int high = n*m-1;
+        while (low <= high) {
+            int mid = ( low + high ) / 2;
+            int row = mid / m;
+            int col = mid % m;
+            if (target == mat[row][col]) return true;
+            else if (target > mat[row][col]) low = mid + 1;
+            else high = mid - 1;
+        }
+        return false;
+    }
+    
 }

9f88f27d1aba8281134bb9d6e97cd70204950d34
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-12-29 12:42:00 +0530

    PrintSpiralMatrix

--- src/com/java/dsa/arrays/PrintSpiralMatrix.java
+++ src/com/java/dsa/arrays/PrintSpiralMatrix.java
@@ -2,6 +2,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrintSpiralMatrix {
+    public static List<Integer> printSpiralNaive(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        // List to store the spiral order elements
+        ArrayList<Integer> result = new ArrayList<>();
+        // 2D array to keep track of visited cells
+        boolean[] visited = new boolean[m][n];
+        // change the row index for each direction
+        int[] dr = {0, 1, 0, -1};
+        // change the column index for each direction
+        int[] dc = {1, 0, -1, 0};
+        // Initial position in the matrix
+        int r=0, c=0;
+        int index = 0;
+        // Iterate through all the elements in the matrix
+        for (int i=0; i<(m*n); ++i) {
+            res.add(mat[r][c]); // add current element to the result list
+            visited[r][c] = true; // Mark current cell as visited
+            int newRow = r + dr[index]; // calculate the next cell coordinates based on current direction
+            int newCol = c + dc[index]; 
+            if (0<=newRow && newRow < m && 0<=newCol && newCol < n && !visited[newRow][newCol]) {
+                r = newRow;
+                c = newCol;
+            } else {
+                index = (index+1)%4; // change direction turn clockwise
+                r += dr[index];
+                c += dc[index];
+            }
+        }
+        return res;
+    }
+
     public static List<Integer> printSpiral(int[][] mat) {
         List<Integer> ans = new ArrayList<>();
         int n = mat.length; // no. of rows
@@ -41,8 +73,16 @@ public class PrintSpiralMatrix {
                        {5, 6, 7, 8},
                        {9, 10, 11, 12},
                        {13, 14, 15, 16}};
+
+        List<Integer> ans = printSpiralNaive(mat);
+
+        for(int i = 0;i<ans.size();i++){
+            System.out.print(ans.get(i) + " ");
+        }
+
+        System.out.println();
         
-        List<Integer> ans = printSpiral(mat);
+        ans = printSpiral(mat);
 
         for(int i = 0;i<ans.size();i++){
             System.out.print(ans.get(i) + " ");

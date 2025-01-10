ea9d9bbffaa9cbe27ab4e8119c598093dd40262d
mounikaperi <saimounikaperi@gmail.com>
2025-01-10 08:14:40 +0530

    Count Pairs

--- src/com/java/dsa/arrays/SumOfDigitsTillSingleDigit.java
+++ src/com/java/dsa/arrays/SumOfDigitsTillSingleDigit.java
@@ -1,2 +1,50 @@
-package com.java.dsa.arrays;public class SumOfDigitsTillSingleDigit {
+package com.java.dsa.arrays;
+
+public class SumOfDigitsTillSingleDigit {
+    public static void main(String[] args) {
+        int n = 1234;
+        System.out.println(singleDigit(n));
+        n = 1234;
+        System.out.println(singleDigitExpected(n));
+    }
+    static int singleDigit(int n) {
+        int sum = 0;
+        while (n>0 || sum > 9) {
+            if (n == 0) {
+                // if n becomes 0, reset it to sum and start a new iteration
+                n = sum;
+                sum = 0;
+            }
+            sum += n % 10;
+            n = n/10;
+        }
+        return sum;
+    }
+    static int singleDigitExpected(int n) {
+        /*
+            We know that every number in the decimal system can be expressed as a sum of its digits multiplied by powers of 10. For example, a number represented as abcd can be written as follows:
+
+            abcd = a*10^3 + b*10^2 + c*10^1 + d*10^0
+
+            We can separate the digits and rewrite this as:
+            abcd = a + b + c + d + (a*999 + b*99 + c*9)
+            abcd = a + b + c + d + 9*(a*111 + b*11 + c)
+
+            This implies that any number can be expressed as the sum of its digits plus a multiple of 9.
+            So, if we take modulo with 9 on each side,
+            abcd % 9 = (a + b + c + d) % 9 + 0
+
+            This means that the remainder when abcd is divided by 9 is equal to the remainder where the sum of its digits (a + b + c + d) is divided by 9.
+
+            If the sum of the digits itself consists of more than one digit, we can further express this sum as the sum
+            of its digits plus a multiple of 9. Consequently, taking modulo 9 will eliminate the multiple of 9, until
+            the sum of digits become single digit number.
+
+            As a result, the sum of the digits of any number, will equal its modulo 9.
+            If the result of the modulo operation is zero, it indicates that the single-digit result is 9.
+         */
+        if (n ==0) return 0;
+        if (n%9 == 0) return 9;
+        return n%9;
+    }
 }
--- src/com/java/dsa/hashing/CountPairs.java
+++ src/com/java/dsa/hashing/CountPairs.java
@@ -1,2 +1,57 @@
-package com.java.dsa.hashing;public class CountPairs {
+package com.java.dsa.hashing;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountPairs {
+    public static void main(String[] args) {
+        int[] arr = { 1, 5, 7, -1, 5 };
+        int target = 6;
+        System.out.println(countPairs(arr, target));
+        System.out.println(countPairsBetter(arr, target));
+        System.out.println(countPairsHashing(arr, target));
+    }
+    static int countPairs(int[] arr, int target) {
+        int n = arr.length;
+        int count = 0;
+        for (int i=0; i<n; i++) {
+            for (int j=i+1; j<n; j++) {
+                if (arr[i] + arr[j] == target) count++;
+            }
+        }
+        return count;
+    }
+    static int countPairsBetter(int[] arr, int target) {
+        Arrays.sort(arr);
+        int count =0, start = 0, end = arr.length-1;
+        while (start < end) {
+            if ((arr[start] + arr[end]) < target) start++;
+            else if ((arr[start] + arr[end]) > target) end--;
+            else {
+                int count1 = 0, count2 = 0;
+                int element1 = arr[start], element2 = arr[end];
+                while (start <= end && arr[start] == element1) {
+                    start++;
+                    count1++;
+                }
+                while(start <= end && arr[end] == element2) {
+                    end--;
+                    count2++;
+                }
+                if (element1 == element2) count += (count1 * (count1-1))/2;
+                else count += (count1*count2);
+            }
+        }
+        return count;
+    }
+    static int countPairsHashing(int[] arr, int target) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        int count = 0;
+        for (int i=0; i<arr.length; i++) {
+            if (freq.containsKey(target-arr[i])) count += freq.get(target-arr[i]);
+            freq.put(arr[i], freq.getOrDefault(arr[i], 0) + 1);
+        }
+        return count;
+    }
 }

10e6c298f73d18232528b0db728c54bbb886697b
mounikaperi <saimounikaperi@gmail.com>
2025-01-10 08:15:15 +0530

    Two Sum and Three Sum

--- src/com/java/dsa/hashing/TripletsWithZeroSum.java
+++ src/com/java/dsa/hashing/TripletsWithZeroSum.java
@@ -1,2 +1,37 @@
-package com.java.dsa.hashing;public class TripletsWithZeroSum {
+package com.java.dsa.hashing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TripletsWithZeroSum {
+    public static void main(String[] args) {
+        int[] arr = {0, -1, 2, -3, 1};
+        ArrayList<ArrayList<Integer>> res = findTriplets(arr);
+        for (List<Integer> triplet : res)
+            System.out.println(triplet.get(0) + " " + triplet.get(1)
+                    + " " + triplet.get(2));
+        res = findTripletsExpected(arr);
+        for (List<Integer> triplet : res)
+            System.out.println(triplet.get(0) + " " + triplet.get(1)
+                    + " " + triplet.get(2));
+    }
+    static ArrayList<ArrayList<Integer>> findTriplets(int[] arr) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        int n = arr.length;
+        for (int i=0; i<n-2; i++) {
+            for (int j=0; j<n-1; j++) {
+                for (int k=0; k<n; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        ArrayList<Integer> triplet = new ArrayList<>();
+                        triplet.add(i); triplet.add(j); triplet.add(k);
+                        result.add(triplet);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+    static ArrayList<ArrayList<Integer>> findTripletsExpected(int[] arr) {
+
+    }
 }
--- src/com/java/dsa/hashing/TwoSum.java
+++ src/com/java/dsa/hashing/TwoSum.java
@@ -1,13 +1,15 @@
-package com.java.dsa.arrays;
+package com.java.dsa.hashing;
 
 import java.util.Arrays;
 import java.util.HashMap;
 public class TwoSum {
 	public static void main(String[] args) {
+		// https://www.geeksforgeeks.org/batch/gfg-160-problems/track/hashing-gfg-160/article/NDg0
 		int[] arr = {2,6,5,8,11};
 		int target = 14;
 		System.out.println("The indices at which elements sum up to target is: " + Arrays.toString(twoSum(arr, target)));
 		System.out.println("The indices at which elements sum up to target is: " + Arrays.toString(twoSumViaHashing(arr, target)));
+		System.out.println(twoSumViaBinarySearch(arr, target));
 	}
 	private static int[] twoSum(int[] arr, int target) {
 	    int[] ans = new int[2];
@@ -36,5 +38,22 @@ public class TwoSum {
 	        map.put(arr[i], i);
 	    }
 	    return ans;
+	}
+	private static boolean twoSumViaBinarySearch(int[] arr, int target) {
+		Arrays.sort(arr);
+		for (int i=0; i<arr.length; i++) {
+			int complement = target - arr[i];
+			if (binarySearch(arr, i+1, arr.length-1, complement)) return true;
+		}
+		return false;
+	}
+	static boolean binarySearch(int[] arr, int left, int right, int target) {
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (arr[mid] == target) return true;
+			else if (target > arr[mid]) left = mid + 1;
+			else right = mid - 1;
+		}
+		return false;
 	}
 }

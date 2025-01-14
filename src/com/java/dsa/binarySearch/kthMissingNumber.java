744029d278b835308d02e05ee40d02dde4289604
mounikaperi <saimounikaperi@gmail.com>
2025-01-14 09:47:09 +0530

    kth Missing number

--- src/com/java/dsa/binarySearch/kthMissingNumber.java
+++ src/com/java/dsa/binarySearch/kthMissingNumber.java
@@ -16,6 +16,19 @@ public class kthMissingNumber {
             else high = mid - 1;
         }
         return low + target;
-    }       
+    }
+    static int kthMissingNaive(int[] arr, int k) {
+
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > (k + i)) {
+                return (k + i);
+            }
+        }
+
+        // If all numbers from 1 to n are present
+        // in arr[], return k + n
+        return k + n;
+    }
 }
 

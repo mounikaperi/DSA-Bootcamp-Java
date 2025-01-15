b6729e7e921c299b0c1de05bc257547f3807a2a3
mounikaperi <saimounikaperi@gmail.com>
2025-01-15 21:27:57 +0530

    Koko eating bananas

--- src/com/java/dsa/binarySearch/KokoEatingBananas.java
+++ src/com/java/dsa/binarySearch/KokoEatingBananas.java
@@ -1,12 +1,29 @@
 package com.java.dsa.binarySearch;
 
+import java.util.Arrays;
+
 public class KokoEatingBananas {
     public static void main(String[] args) {
       // https://leetcode.com/problems/koko-eating-bananas/description/
       int[] piles = {3,6,7,11};
       int h = 8;
-      int minimumTime = minEatingSpeed(piles, h);
+      int minimumTime = maxEatingSpeedNaive(piles, h);
       System.out.println("Minimum number of hours needed for koko to eat all bananas: " + minimumTime);
+      int minimumTime1 = minEatingSpeed(piles, h);
+      System.out.println("Minimum number of hours needed for koko to eat all bananas: " + minimumTime1);
+    }
+    static int maxEatingSpeedNaive(int[] piles, int h) {
+        int max = Arrays.stream(piles).max().getAsInt();
+        for (int speed=1; speed<=max; speed++) {
+            int time = 0;
+            for (int bananas: piles) {
+                time += bananas / speed; // time required to eat this pile of bananas at current speed
+                if (bananas % speed != 0) // 1 extra hour to eat this remainder number of bananas in this pile
+                    time++;
+            }
+            if (time <=h) return speed;
+        }
+        return max;
     }
     private static int minEatingSpeed(int[] piles, int h) {
       int low = 1;

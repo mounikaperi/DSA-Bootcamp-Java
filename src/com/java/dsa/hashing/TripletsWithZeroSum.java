b475842a590057a95d463c429a15bb7e96c2d041
mounikaperi <saimounikaperi@gmail.com>
2025-01-16 19:15:45 +0530

    Triplets with Zero Sum

--- src/com/java/dsa/hashing/TripletsWithZeroSum.java
+++ src/com/java/dsa/hashing/TripletsWithZeroSum.java
@@ -1,37 +1,60 @@
 package com.java.dsa.hashing;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class TripletsWithZeroSum {
     public static void main(String[] args) {
         int[] arr = {0, -1, 2, -3, 1};
-        ArrayList<ArrayList<Integer>> res = findTriplets(arr);
+        List<List<Integer>> res = findTriplets(arr);
         for (List<Integer> triplet : res)
             System.out.println(triplet.get(0) + " " + triplet.get(1)
                     + " " + triplet.get(2));
-//        res = findTripletsExpected(arr);
+        res = findTripletsExpected(arr);
         for (List<Integer> triplet : res)
             System.out.println(triplet.get(0) + " " + triplet.get(1)
                     + " " + triplet.get(2));
     }
-    static ArrayList<ArrayList<Integer>> findTriplets(int[] arr) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+    static List<List<Integer>> findTriplets(int[] arr) {
         int n = arr.length;
+        List<List<Integer>> tripletsList = new ArrayList<>();
         for (int i=0; i<n-2; i++) {
-            for (int j=0; j<n-1; j++) {
-                for (int k=0; k<n; k++) {
-                    if (arr[i] + arr[j] + arr[k] == 0) {
-                        ArrayList<Integer> triplet = new ArrayList<>();
-                        triplet.add(i); triplet.add(j); triplet.add(k);
-                        result.add(triplet);
+            for (int j=i+1; j<n-1; j++) {
+                for (int k = j+1; k<n; k++) {
+                    if ((arr[i] + arr[j] + arr[k]) == 0) {
+                        List<Integer> currentList = new ArrayList<>();
+                        currentList.add(i);
+                        currentList.add(j);
+                        currentList.add(k);
+                        tripletsList.add(currentList);
                     }
                 }
             }
         }
-        return result;
+        return tripletsList;
     }
-//    static ArrayList<ArrayList<Integer>> findTripletsExpected(int[] arr) {
-//
-//    }
+    static List<List<Integer>> findTripletsExpected(int[] arr) {
+        int n = arr.length;
+        Set<List<Integer>> tripletsList = new HashSet<>();
+        Map<Integer, List<int[]>> map = new HashMap<>();
+        for (int i=0; i<n; i++) {
+            for (int j=i+1; j<n; j++) {
+                map.computeIfAbsent(arr[i] + arr[j], k -> new ArrayList<>()).add(new int[] {i, j});
+            }
+        }
+        for (int i=0; i<n; i++) {
+            int remainingSum = -arr[i];
+            if (map.containsKey(remainingSum)) {
+                List<int[]> pairs = map.get(remainingSum);
+                for (int[] p: pairs) {
+                    if (p[0] !=i && p[1]!=i) {
+                        List<Integer> currentList = new ArrayList<>(Arrays.asList(i, p[0], p[1]));
+                        Collections.sort(currentList);
+                        tripletsList.add(currentList);
+                    }
+                }
+            }
+        }
+        List<List<Integer>> finalList = new ArrayList<>(tripletsList);
+        return finalList;
+    }
 }
